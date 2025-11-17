bafe3c95ee5d1b7a220dd72700d18e245e7b8c75
it's kunal_riku <140656671+kunaldas7250@users.noreply.github.com>
2025-11-17 18:06:23 +0530

    Update String 60 dsa interview.js

--- src/Dsa Javascript/String 60 dsa interview.js
+++ src/Dsa Javascript/String 60 dsa interview.js
@@ -8,3 +8,26 @@ function reverse(str){
 }
 const name="kunal"
 console.log(reverse(name))
+
+
+//Check if a string is a palindrome
+function palindrome(str){
+  const copy=str
+  const result=copy.split("")
+  let flag=true
+  for(let i=0;i<result.length;i++){
+    let j=result.length-1-i
+    if(result[i]!==result[j]){
+      flag=false
+      break
+    }
+  }
+  if(flag==true){
+    console.log("is palindrome")
+  }
+  else{
+    console.log("not palindrome ")
+  }
+}
+const name="abc"
+console.log(palindrome(name))
